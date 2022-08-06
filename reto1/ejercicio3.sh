2d2721ba1bc40276788edd46bfe399ea93d70d5a
starmaster1990 <ronny.tigrero.22@gmail.com>
2022-08-06 01:20:56 -0500

    ➕ ADD: reto 1
    Se reliza soluciones a ejercicios del reto 1
    
    ISSUE: 1

---
+++ reto1/ejercicio3.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+######## Ejercicio 1 ########
+basePath="foo"
+dir1="$basePath/dummy"
+dir2="$basePath/empty"
+file1="$dir1/file1.txt"
+file2="$dir1/file2.txt"
+######## Solo esta linea es del - Ejercicio 3 ########
+string=${1:-"Que me encanta bash!!"}
+
+<< 'Comment'
+    Elimina el directorio para volver a recrear todo de nuevo
+Comment
+deleteDirectory(){
+    rm -rf "$basePath"
+}
+
+<< 'Comment'
+    Crea directorio
+    Recibe 1 parametro - el nombre del directorio
+Comment
+directoryCreator(){
+    mkdir -p $1
+}
+
+deleteDirectory
+
+directoryCreator $dir1
+directoryCreator $dir2
+
+echo "$string" > "$file1"
+touch "$file2"
+
+######## Ejercicio 2 ########
+
+cat  "$file1" > "$file2"
+mv "$file2" "$dir2"
+
+
